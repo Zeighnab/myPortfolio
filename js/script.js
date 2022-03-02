270a55ab4ca123bf0db98fd523dfe3a4ee9cba52
Zainab <olaniyanzainab@gmail.com>
2022-03-02 14:10:05 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@ const navBtn = document.getElementById('navbtn');
 const sideBar = document.getElementById('sidebar');
 const navBar = document.getElementById('navbar');
 const closeBtn = document.getElementById('closebtn');
-const sideLinks = document.querySelectorAll('.side')
+const sideLinks = document.querySelectorAll('.side');
 
 window.addEventListener('scroll', fixNav);
 
 function fixNav() {
-  if (window.scrollY > navBar.offsetHeight) {
+  if (window.scrollY >= navBar.offsetHeight) {
     navBar.classList.add('active');
   } else {
     navBar.classList.remove('active');
   }
 }
-
 
 navBtn.addEventListener('click', () => {
   sideBar.classList.add('show-sidebar');
@@ -23,8 +22,8 @@ closeBtn.addEventListener('click', () => {
   sideBar.classList.remove('show-sidebar');
 });
 
-sideLinks.forEach(side => {
+sideLinks.forEach((side) => {
   side.addEventListener('click', () => {
-    sideBar.classList.remove('show-sidebar')
-  })
-})
+    sideBar.classList.remove('show-sidebar');
+  });
+});
